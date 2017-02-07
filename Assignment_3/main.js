ce171826e3d353fd0d04e71fe8c3fe45b363165f
Cirrith <Ryan.bambrough@gmail.com>
2017-02-06 18:04:46 -0600

    Drawing circle

--- Assignment_3/main.js
+++ Assignment_3/main.js
@@ -10,6 +10,7 @@ function init() {
 		moveToTx(0,0,0,Tx);lineToTx(50,0,0,Tx);cxt.stroke();
 		moveToTx(0,0,0,Tx);lineToTx(0,150,0,Tx);cxt.stroke();
 		moveToTx(0,0,0,Tx);lineToTx(0,0,250,Tx);cxt.stroke();
+		arcTx(100,100,0,25, 0, 2*Math.PI, Tx);cxt.fill();
 	}
 
 	function moveToTx(x,y,z,Tx) {
@@ -24,16 +25,24 @@ function init() {
 		cxt.lineTo(locTx[0]+250,-locTx[1]+250);
 	}
 	
+	function arcTx(x,y,z,radius,sAngle,eAngle,Tx) {
+		var loc = [x,y,z];
+		var locTx = m4.transformPoint(Tx, loc);
+		moveToTx(x,y,z,Tx)
+		cxt.arc(locTx[0]+250,-locTx[1]+250,radius, sAngle, eAngle);
+	}
+	
 	var canvas = document.getElementById("canvas");
 	var cxt = canvas.getContext('2d');
 	
 	var eye = [500, 200, 200];
 	var target = [0, 0, 0];
-	var up = [0, 1, 0];
+	var up = [0, 0, 1];
 	
 	var Tcamera=m4.inverse(m4.lookAt(eye, target, up));
 	
 	drawAxes(Tcamera);
+	
 	// var test = new Firework();
 	
 	
