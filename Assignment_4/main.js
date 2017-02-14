1a4a6a3ba0211b10d9ca1523c5b6df094576c279
Cirrith <Ryan.bambrough@gmail.com>
2017-02-14 09:29:30 -0600

    Transform Problem

--- Assignment_4/main.js
+++ Assignment_4/main.js
@@ -55,14 +55,41 @@ function update() {
 	eye = [radius*Math.cos(theta), radius*Math.sin(theta), 20];
 	Tcamera=m4.inverse(m4.lookAt(eye, target, up));
 	
+	var projM = m4.perspective(Math.PI/2, 1, 0.1, 100);
+	
+		var viewport = m4.scaling([xsize/2,-ysize/2,1]);
+		m4.setTranslation(viewport,[xsize/2,ysize/2,0],viewport);
+
+		// get the projection
+		var projM;
+		if (mySliders.perspective.value > 0) {
+			var fov = toRadians(mySliders.fov.value);
+			projM = m4.perspective(fov, 1, 0.1, 100);
+		} else {
+			projM = m4.scaling([.1,.1,1]);
+		}
+		// don't forget... lookat give the CAMERA matrix, not the view matrix
+		var lookAtPt = [mySliders.lookAtX.value, mySliders.lookAtY.value, mySliders.lookAtZ.value];
+		var lookFromPt = [mySliders.lookFromX.value, mySliders.lookFromY.value, mySliders.lookFromZ.value];
+		var lookatI = m4.lookAt(lookFromPt, lookAtPt, [0,1,0]);
+		var lookatM = m4.inverse(lookatI);
+
+		// the whole transform
+        var viewii = m4.multiply(ab.getMatrix(),lookatM);
+		var viewi = m4.multiply(viewii,projM);
+		var view = m4.multiply(viewi,viewport);
+	
+	
+	var Tinter = m4.multiply(Tcamera, scale);
+	
 	for(var i=0; i<gridSize; i++) {
 		for(var j=0; j<gridSize; j++) {
-			var Trans = m4.translate(scale, [i-gridSize/2, j-gridSize/2,0]);
-			var Tview = m4.multiply(Tcamera, Trans);
+			var Trans = m4.translate(m4.identity(), [i-gridSize/2, j-gridSize/2,0]);
+			var Tnew = m4.multiply(Trans, Tinter);
 			if(i%2 == j%2) {
-				paint.addSquare("black", 1, "Grid",Tview);
+				paint.addSquare("black", 1, "Grid",Tnew);
 			} else {
-				paint.addSquare("black", 0.8, "Grid",Tview);
+				paint.addSquare("black", 0.8, "Grid",Tnew);
 			}
 		}
 	}
