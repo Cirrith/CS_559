91962ac9f3c9a8c4607d99d37210119c06860c39
Cirrith <Ryan.bambrough@gmail.com>
2017-02-15 19:09:06 -0600

    Need to change overlap

--- Assignment_4/main.js
+++ Assignment_4/main.js
@@ -7,12 +7,12 @@ var paint;
 
 var theta = 0;
 var dtheta = 0.01;
-var radius = 200;
-var eyeHeight = 100;
+var radius = 20;
+var eyeHeight = 400;
 var target = [0,0,0];
 var up = [0,0,1];
 
-var gridSize = 50;
+var gridSize = 20;
 var wire = false;
 
 // Transform from world to camera
@@ -28,11 +28,6 @@ function init() {
 	canvas = document.getElementById("canvas");
 	cxt = canvas.getContext('2d');
 	paint = new Painter(canvas, cxt);
-	
-	Tscale = m4.scaling([25,25,25]);
-	Tproj = m4.identity(); //m4.perspective(Math.PI/2, 1, 5, 400);
-	Tview = m4.identity(); //m4.multiply(m4.scaling([canvas.width/2,-canvas.height/2,1]), m4.translation([canvas.width/2,canvas.height/2,0]));
-	Tlate = m4.multiply(Tproj, Tview);
 	
 	window.requestAnimationFrame(update);
 }
@@ -53,76 +48,38 @@ function drawCube(x,y,z,color,Tx) {
 function update() {
 	"use strict";
 	cxt.clearRect(0,0,canvas.width,canvas.height);  // Clean Canvas
+	cxt.fillStyle = "Black";
+	cxt.fillRect(0,0,canvas.width,canvas.height);
 	theta += dtheta;  // Increment rotation
 
 	var eye = [radius*Math.cos(theta), radius*Math.sin(theta), eyeHeight];
 	var Tcamera=m4.inverse(m4.lookAt(eye, target, up));
+
+	var Tscale = m4.scaling([25,25,25]);
 	
-	//var Tmodel = m4.multiply(m4.scaling([50,50,50]),m4.translation([1,0,0]));
-	var Tproj = m4.perspective(Math.PI/3,2,5,200);
-	//var Tviewport = m4.multiply(m4.scaling([canvas.width/2,canvas.height/2,1]),m4.translation([canvas.width/2,canvas.height/2,0]));
-	//var Tvp = m4.multiply(m4.translation([canvas.width/2,-canvas.height/2,0]),m4.scaling([canvas.wdith/2,canvas.height/2,1]));
-	
-	var Tproj = m4.perspective(Math.PI/3,2,0,400);
+	var Tproj = m4.perspective(Math.PI/4,canvas.width/canvas.height,50,100);
 	var Tvpscale = m4.scaling([canvas.width/2,-canvas.height/2,1]);
 	var Tvptrans = m4.translation([canvas.width/2,canvas.height/2,0]);
 	
 	var Tvp = m4.multiply(Tvptrans,Tvpscale);
-	
-	var Trb = m4.translation([1,0,0]);
-	var Trr = m4.translation([0,0,0]);
-	//Tscale->Tcamera->Trans
-	//var Tview = m4.multiply(Tscale, Tcamera);
-	//var Tsqb = m4.multiply(Tview, Trb);
-	//var Tsqr = m4.multiply(Tview, Trr);
-	
-	//Tcamera->Tscale->Trans
-	var Tview = m4.multiply(Tcamera, Tscale);
-	var Tsqb = m4.multiply(Tview,Trb);
-	var Tsqr = m4.multiply(Tview,Trr);
-	
-	//Tprojection->Tcamera->Tscale->Trans
-	//var Tviewii = m4.multiply(Tcamera, Tscale);
-	//var Tviewi = m4.multiply(Tproj,Tviewii);
-	//var Tview = m4.multiply(Tvp,Tviewi);
-	//var Tsqb = m4.multiply(Tview,Trb);
-	//var Tsqr = m4.multiply(Tview,Trr);
-	
-	//Trans->Tscale->Tcamera->Tprojection
-	//var Tmodel = m4.multiply(Trr,Tscale);
-	//var Tviewii = m4.multiply(Tmodel,Tcamera);
-	//var Tviewi = m4.multiply(Tviewii,Tproj);
-	//var Tview = m4.multiply(Tviewi,m4.multiply(Tvpscale,Tvptrans));
-	
+
 	//Tscale->Tcamera->Tproj->Tvp
 	var Tviewii = m4.multiply(Tscale,Tcamera);
 	var Tviewi = m4.multiply(Tviewii,Tproj);
 	var Tbasic = m4.multiply(Tviewi,m4.multiply(Tvpscale,Tvptrans));
 	
-	//var Tviewii = m4.multiply(Tscale,Tcamera);
-	//var Tviewi = m4.multiply(Tviewii,Tproj);
-	//var Tbasic = m4.multiply(Tviewi,m4.multiply(Tvpscale,Tvptrans));
-	
-	var Tviewr = m4.multiply(Trr,Tbasic);
-	var Tviewb = m4.multiply(Trb,Tbasic);
-	
-	paint.addSquare("red", 1, "Grid", Tviewr);
-	paint.addSquare("black", 1, "Grid", Tviewb);
-	
-	
-	/*for(var i=0; i<gridSize; i++) {
+	for(var i=0; i<gridSize; i++) {
 		for(var j=0; j<gridSize; j++) {
 			var Trans = m4.translation([i-gridSize/2, j-gridSize/2,0]);
-			var Tinter = m4.multiply(Tscale,Trans)
-			var Tnew = m4.multiply(Trans, Tnope);
+			var Tgrid = m4.multiply(Trans,Tbasic); 
 			
 			if(i%2 == j%2) {
-				paint.addSquare("black", 1, "Grid",Tnew);
+				paint.addSquare("black", 1, "Grid",Tgrid);
 			} else {
-				paint.addSquare("black", 0.8, "Grid",Tnew);
+				paint.addSquare("black", 0.8, "Grid",Tgrid);
 			}
 		}
-	}*/
+	}
 	//drawCube(10,0,1,"red", Tview);
 	//drawCube(0,0,1,"blue", Tview);
 	
