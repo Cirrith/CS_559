916795f2495d028729ba7ab5054b3c21e5b45f38
Cirrith <Ryan.bambrough@gmail.com>
2017-03-11 13:43:18 -0600

    Nope

--- Assignment_6/main.js
+++ Assignment_6/main.js
@@ -1,67 +1,65 @@
+// draw a triangle using WebGL
+// write everything out, step at a time
+//
+// written by gleicher on October 3, 2015
+
 function start() {
-	var m4 = twgl.m4;
+  "use strict";
+
+  // first we need to get the canvas and make an OpenGL context
+	// in practice, you need to do error checking
 	var canvas = document.getElementById("canvas");
-	var gl = twgl.getWebGLContext(canvas);
-	
-	var target = [0,0,0];  // Always Look at Center
-	var up = [0,0,1];  // Z is always up
-	var eye = [1, 3, -20];
-	
-	//Transforms
-		// Perspective -> Scale -> Translate
-	var Tproj =  m4.perspective(Math.PI/2,canvas.width/canvas.height,0.5,100);
-	var Tvpscale = m4.scaling([canvas.width/2,-canvas.height/2,1]);
-	var Tvptrans = m4.translation([canvas.width/2,canvas.height/2,0]);
-	var Tview = m4.multiply(m4.multiply(Tvpscale,Tvptrans),Tproj);
-	var Tcamera = m4.inverse(m4.lookAt(eye,target,up));  // Camera
-	var TMat = m4.multiply(Tcamera,Tview);
-	
-	// WebGL
-	var shader = twgl.createProgramInfo(gl, ["vs", "fs"]);
-	
-	var sphere = twgl.primitives.createSphereBufferInfo(gl, 1, 24, 12);
+	var gl =twgl.getWebGLContext(canvas);
 
-	
-	function draw() {
-		gl.clearColor(1.0,1.0,1.0,1.0);  // Clear color is black
-		gl.enable(gl.DEPTH_TEST);  // Enable z-buffer
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);  // Clear Screen and Depth
-		
-		gl.useProgram(shader.program);
-		twgl.setUniforms(shader, {transform: TMat});
-		twgl.setBuffersAndAttributes(gl,shader,sphere);
-		twgl.drawBufferInfo(gl, gl.TRIANGLES, sphere);
-		// Draw Stuff
-		
-		//gl.drawArrays(gl.TRIANGLES, 0, sphere.numItems);
-		//gl.drawElements(gl.TRIANGLES, sphere.indices.length, gl.UNSIGNED_SHORT, 0);
-		//window.requestAnimationFrame(draw);
-	}
-	
-	
-	// Mouse Stuff - Ignore until drawing	
-	var mouseDown = false;
-	var lastMouseX;
-	var lastMouseY;
+  // with twgl, the shaders get moved to the html file
+  var shaders = twgl.createProgramInfo(gl, ["vs", "fs"]);
+  
+  // let's define the vertex positions
+  var vertexPos = [
+         0.0,  1.0,  0.0,
+        -1.0,  0.0,  0.0,
+         0.5,  0.0,  0.0,
+         0.0, -1.0,  0.0,
+        -0.5,  0.0,  0.0,
+         1.0,  0.0,  0.0,
+   ];  
+  var vertexColors = [
+    1.0, 1.0, 0.0,
+    1.0, 1.0, 0.0,
+    1.0, 1.0, 0.0,
+    1.0, 0.0, 1.0,    
+    1.0, 0.0, 1.0,    
+    1.0, 0.0, 1.0    
+    ];
+  var arrays = {pos:vertexPos, 
+                inColor:vertexColors};
+  var buffers = twgl.createBufferInfoFromArrays(gl, arrays);
+  
+  // this is the "draw scene" function, but since this 
+  // is execute once...
+  
+  var theta = 0.25;
+  
+  function draw() {
+	  // first, let's clear the screen
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+	  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	canvas.onmousedown = mouseDownHandler;
-	document.onmouseup = mouseUpHandler;
-	document.onmousemove = mouseMoveHandler;
-
-	
-	function mouseDownHandler(e) {
-		mouseDown = true;
-		lastMouseX = e.x;
-		lastMouseY = e.y;
-	}
-	
-	function mouseUpHandler(e) {
-		mouseDown = false;
-	}
-	
-	function mouseMoveHandler(e) {
-	}
-	draw();
+    // now we draw the triangle(s)
+    // we tell GL what program to use, and what memory block
+    // to use for the data, and that the data goes to the pos
+    // attribute
+    gl.useProgram(shaders.program);	    
+  
+    var mytrans = twgl.m4.rotationZ(theta);
+    twgl.setUniforms(shaders,{transf : mytrans});
+    twgl.setBuffersAndAttributes(gl,shaders,buffers);
+    twgl.drawBufferInfo(gl, gl.TRIANGLES, buffers);
+    theta += 0.01;
+    window.requestAnimationFrame(draw);
+  }
+  draw();
 }
-
-window.onload=start;
+start();
+  
