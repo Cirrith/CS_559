9311d7805c541c12429fb99b1dd72673c7062072
Cirrith <Ryan.bambrough@gmail.com>
2017-03-11 15:17:05 -0600

    Drawing

--- Assignment_6/main.js
+++ Assignment_6/main.js
@@ -7,66 +7,67 @@ function start() {
 	var up = [0,0,1];  // Z is always up
 	var eye = [10, 0, 10];
 	
-	gl.clearColor(1.0,1.0,1.0,1.0);  // Clear color is black
-	gl.enable(gl.DEPTH_TEST);  // Enable z-buffer
+	//Transforms	
+	var tModel = m4.identity();
+	var tCamera = m4.inverse(m4.lookAt(eye, target, up));
+	var tProjection = m4.perspective(Math.PI/2, canvas.width/canvas.height, 10, 1000);
 	
-	//Transforms
-		// Perspective -> Scale -> Translate
-	var Tproj =  m4.perspective(Math.PI/2,canvas.width/canvas.height,5,10);
-	var Tvpscale = m4.scaling([canvas.width/2,-canvas.height/2,1]);
-	var Tvptrans = m4.translation([canvas.width/2,canvas.height/2,0]);
-	var Tview = m4.multiply(m4.multiply(Tvpscale,Tvptrans),Tproj);
+	var tMat = m4.multiply(m4.multiply(tModel,tCamera), tProjection);
 
-	var Tcamera = m4.inverse(m4.lookAt(eye,target,up));  // Camera
-	
-	var TMat = m4.multiply(Tcamera,Tview);
-	
-
-	// Shaders
+	// Shaders and Program
 	var shader = twgl.createProgramInfo(gl, ["vs", "fs"]);  // Compile and link program
-	
-	gl.useProgram(shader.program);
-	
 	if(shader == null) {
 		alert("Compile Failed");
 		return;
 	}
 	
+	gl.useProgram(shader.program);
+	
 	// Objects
 	var sphere = twgl.primitives.createSphereVertices(1, 24, 12);  // Create sphere
-		// indices, normal, position, texcoord
 	
 	// Buffers
+		// Position
 	sphere.posBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, sphere.posBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, sphere.position, gl.STATIC_DRAW);  // Let Data flow
 	sphere.itemSize = 3;
 	sphere.numItems = sphere.position.length/3;
+		// Normal
+	//sphere.normBuffer = gl.createBuffer();
+	//gl.bindBuffer(gl.ARRAY_BUFFER, sphere.normBuffer);
+	//gl.bufferData(gl.ARRAY_BUFFER, sphere.normal, gl.STATIC_DRAW);
+
 	
 	sphere.indexBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sphere.indexBuffer);
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, sphere.indices, gl.STATIC_DRAW);
 	
 	// Setup Locations
+		// Position Attribute
 	shader.posAttribute = gl.getAttribLocation(shader.program,"pos");
 	gl.enableVertexAttribArray(shader.posAttribute);
-	shader.transUniform = gl.getUniformLocation(shader.program, "projectionMatrix");
-	
-	// Setup uniform
-	gl.uniformMatrix4fv(shader.transUniform, false, TMat);
-	
-	// Setup Attribute
-	gl.bindBuffer(gl.ARRAY_BUFFER, sphere.posBuffer);
-	gl.vertexAttribPointer(shader.posAttribute, sphere.itemSize, gl.FLOAT, false, 0, 0);
+		// Transform Uniform
+	shader.transUniform = gl.getUniformLocation(shader.program, "tProj");
+
+	gl.clearColor(1.0,1.0,1.0,1.0);  // Clear color is black
+	gl.enable(gl.DEPTH_TEST);  // Enable z-buffer
 	
 	function draw() {
+
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);  // Clear Screen and Depth
+
+		// Setup Attributes and Uniform
+		gl.uniformMatrix4fv(shader.transUniform,false,tMat);  // Uniform
+		
+		gl.bindBuffer(gl.ARRAY_BUFFER, sphere.posBuffer);
+		gl.vertexAttribPointer(shader.posAttribute,sphere.itemSize, gl.FLOAT, false, 0, 0);
 		
 		// Draw Stuff
-		
-		//gl.drawArrays(gl.TRIANGLES, 0, sphere.numItems);
 		gl.drawElements(gl.TRIANGLES, sphere.indices.length, gl.UNSIGNED_SHORT, 0);
-		//window.requestAnimationFrame(draw);
+		
+		
+		window.requestAnimationFrame(draw);
 	}
 	
 	
