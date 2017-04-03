83398d4c9fc8bf0aa30376186d60e565ab5fc8e4
Cirrith <Ryan.bambrough@gmail.com>
2017-04-03 14:01:44 -0500

    Ship+Sphere Redo

---
+++ Assignment_7/Sphere/sphere.js
@@ -0,0 +1,224 @@
+/******************************************************************************
+File Name: 
+	ship.js
+
+Purpose: 
+	Define and add a sphere object to the graphicstown grobojects list
+
+Requires:
+	name
+	init
+	draw
+	center
+******************************************************************************/
+
+var grobjects = grobjects || [];
+
+var Sphere = undefined;
+
+(function() {
+    "use strict";
+
+	var sphere_data = new Sphere_Data();
+	var shaderProgram = undefined;
+	
+	Sphere = function Sphere(name, position, size, color) {
+		this.name = name;
+		this.position = position || [0,0,0];
+		this.size = size || 1.0;
+		this.color = color || [0.85,0.85,0.85];
+	}
+	
+	Sphere.prototype.init = function(drawingState) {
+		var gl = drawingState.gl;
+
+        // Compile Vertex Shader
+        var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+        gl.shaderSource(vertexShader,sphere_data.vs);
+        gl.compileShader(vertexShader);
+        if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+			alert(gl.getShaderInfoLog(vertexShader));
+            return null;
+        }
+        
+		// Compile Fragment Shader
+        var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(fragmentShader,sphere_data.fs);
+        gl.compileShader(fragmentShader);
+        if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+			alert(gl.getShaderInfoLog(fragmentShader));
+            return null;
+        }
+
+		// Create Shader Program
+        this.shaderProgram = gl.createProgram();
+        gl.attachShader(this.shaderProgram, vertexShader);
+        gl.attachShader(this.shaderProgram, fragmentShader);
+        gl.linkProgram(this.shaderProgram);
+        if (!gl.getProgramParameter(this.shaderProgram, gl.LINK_STATUS)) {
+            alert("Could not initialize shaders");
+        }
+
+		// Setup Attribute Locations
+        this.posLoc = gl.getAttribLocation(this.shaderProgram, "pos");
+        this.normLoc = gl.getAttribLocation(this.shaderProgram, "norm");
+        this.normMatLoc = gl.getUniformLocation(this.shaderProgram,"normMat");
+        this.viewMatLoc = gl.getUniformLocation(this.shaderProgram,"viewMat");
+		this.projMatLoc = gl.getUniformLocation(this.shaderProgram,"projMat");
+		this.sunLoc = gl.getUniformLocation(this.shaderProgram,"sunLoc");
+
+        // Buffer Data
+			// Position
+        this.posBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.posBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, sphere_data.position, gl.STATIC_DRAW);
+			// Normal
+        this.normBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.normBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, sphere_data.normal, gl.STATIC_DRAW);
+			// Index
+		this.indexBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, sphere_data.index, gl.STATIC_DRAW);
+	}
+	
+	Sphere.prototype.draw = function(drawingState) {
+		var gl = drawingState.gl;
+		// choose the shader program we have compiled
+		gl.useProgram(this.shaderProgram);
+
+		// enable the attributes we had set up
+		gl.enableVertexAttribArray(this.posLoc);
+		gl.enableVertexAttribArray(this.normLoc);
+
+		var position = twgl.m4.translation([0,0.5,0]);
+		var scale = twgl.m4.scaling([0.25,0.25,0.25]);
+		var modelM = twgl.m4.multiply(twgl.m4.multiply(scale,position),drawingState.view);
+
+		// set the uniforms
+		gl.uniformMatrix4fv(this.normMatLoc,false,twgl.m4.inverse(twgl.m4.transpose(modelM)));
+		gl.uniformMatrix4fv(this.viewMatLoc,false,modelM);
+		gl.uniformMatrix4fv(this.projMatLoc,false,drawingState.proj);
+		gl.uniform3fv(this.sunLoc,drawingState.sunDirection);
+
+		// connect the attributes to the buffer
+		gl.bindBuffer(gl.ARRAY_BUFFER,this.posBuffer);
+		gl.vertexAttribPointer(this.posLoc,sphere_data.itemSize, gl.FLOAT, false, 0, 0);
+		gl.bindBuffer(gl.ARRAY_BUFFER,this.normBuffer);
+		gl.vertexAttribPointer(this.normLoc,sphere_data.itemSize, gl.FLOAT, false, 0, 0);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+		gl.drawElements(gl.TRIANGLES, sphere_data.index.length,gl.UNSIGNED_SHORT, 0);
+	}
+	
+	Sphere.prototype.center = function(drawingState) {
+		return [0,-.5,0];
+	}
+	
+	
+		// Define ship object
+    var sphere = {
+        name : "sphere",
+
+		   // constructor for Cubes
+		Cube = function Cube(name, position, size, color) {
+			this.name = name;
+			this.position = position || [0,0,0];
+			this.size = size || 1.0;
+			this.color = color || [.7,.8,.9];
+		}
+		
+        init : function(drawingState) {
+            var gl = drawingState.gl;
+
+            // Compile Vertex Shader
+            var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+            gl.shaderSource(vertexShader,sphere_data.vs);
+            gl.compileShader(vertexShader);
+              if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+                      alert(gl.getShaderInfoLog(vertexShader));
+                      return null;
+                  }
+            // Compile Fragment Shader
+            var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+            gl.shaderSource(fragmentShader,sphere_data.fs);
+            gl.compileShader(fragmentShader);
+            if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+                  alert(gl.getShaderInfoLog(fragmentShader));
+                  return null;
+            }
+			
+			// Create Shader Program
+            this.shaderProgram = gl.createProgram();
+            gl.attachShader(this.shaderProgram, vertexShader);
+            gl.attachShader(this.shaderProgram, fragmentShader);
+            gl.linkProgram(this.shaderProgram);
+            if (!gl.getProgramParameter(this.shaderProgram, gl.LINK_STATUS)) {
+                alert("Could not initialize shaders");
+            }
+
+			// Setup Attribute Locations
+            this.posLoc = gl.getAttribLocation(this.shaderProgram, "pos");
+            this.normLoc = gl.getAttribLocation(this.shaderProgram, "norm");
+            this.normMatLoc = gl.getUniformLocation(this.shaderProgram,"normMat");
+            this.viewMatLoc = gl.getUniformLocation(this.shaderProgram,"viewMat");
+			this.projMatLoc = gl.getUniformLocation(this.shaderProgram,"projMat");
+			this.sunLoc = gl.getUniformLocation(this.shaderProgram,"sunLoc");
+
+            // Buffer Data
+				// Position
+            this.posBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.posBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, sphere_data.position, gl.STATIC_DRAW);
+				// Normal
+            this.normBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.normBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, sphere_data.normal, gl.STATIC_DRAW);
+				// Index
+			this.indexBuffer = gl.createBuffer();
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+			gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, sphere_data.index, gl.STATIC_DRAW);
+			
+        },
+        draw : function(drawingState) {
+            var gl = drawingState.gl;
+            // choose the shader program we have compiled
+            gl.useProgram(this.shaderProgram);
+
+            // enable the attributes we had set up
+            gl.enableVertexAttribArray(this.posLoc);
+            gl.enableVertexAttribArray(this.normLoc);
+
+			var position = twgl.m4.translation([0,0.5,0]);
+			var scale = twgl.m4.scaling([0.25,0.25,0.25]);
+			var modelM = twgl.m4.multiply(twgl.m4.multiply(scale,position),drawingState.view);
+			
+            // set the uniforms
+            gl.uniformMatrix4fv(this.normMatLoc,false,twgl.m4.inverse(twgl.m4.transpose(modelM)));
+            gl.uniformMatrix4fv(this.viewMatLoc,false,modelM);
+			gl.uniformMatrix4fv(this.projMatLoc,false,drawingState.proj);
+			gl.uniform3fv(this.sunLoc,drawingState.sunDirection);
+
+            // connect the attributes to the buffer
+            gl.bindBuffer(gl.ARRAY_BUFFER,this.posBuffer);
+            gl.vertexAttribPointer(this.posLoc,sphere_data.itemSize, gl.FLOAT, false, 0, 0);
+            gl.bindBuffer(gl.ARRAY_BUFFER,this.normBuffer);
+            gl.vertexAttribPointer(this.normLoc,sphere_data.itemSize, gl.FLOAT, false, 0, 0);
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+			gl.drawElements(gl.TRIANGLES, sphere_data.index.length,gl.UNSIGNED_SHORT, 0);
+        },
+        center : function(drawingState) {
+            return [0,-.5,0];
+        },
+
+        shaderProgram : undefined,
+        posBuffer : undefined,
+        colorBuffer : undefined,
+        posLoc : -1,
+        colorLoc : -1,
+        projLoc : -1,
+        viewLoc : -1
+    };
+
+    // Add Object to drawing list
+    grobjects.push(sphere);
+})();
